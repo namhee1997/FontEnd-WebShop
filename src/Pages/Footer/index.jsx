9964ed0c2a12df87afb5eaf85c84aa70e2c6e986
Nam-Nee <namkaka2612@gmail.com>
2022-07-08 20:18:58 +0700

    update footer

--- src/Pages/Footer/index.jsx
+++ src/Pages/Footer/index.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer>
             <div className="box_footer">
                 <div className="container_main">
-                    <h3>Design by: Nam Hee - Using: reactjs - nodejs</h3>
+                    <h3>Design by: Nam Hee - Using: reactjs - nodejs (trang được viết trong thời gian ngắn nên chưa hoàn thiện đầy đủ mong anh chị thông cảm😀)</h3>
                 </div>
             </div>
         </footer>
